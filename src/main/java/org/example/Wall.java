e84cb8ff664aad4dbad668cbf8923994777ca05a
Mateusz Murak <mateuszmurak@yahoo.com>
2023-06-16 19:33:31 +0200

    Errors eliminated, implementeations empty (not stable, method of not null type return null).

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -1,7 +1,21 @@
 package org.example;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Wall implements Structure {
     private List<Block> blocks;
+
+    public Optional<Block> findBlockByColor(String color) {
+        return null;
+    }
+
+    public List<Block> findBlocksByMaterial(String material) {
+
+        return null;
+    }
+
+    public int count() {
+        return 0;
+    }
 }
