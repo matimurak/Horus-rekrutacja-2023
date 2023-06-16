ff5278c240a227c6c0e249aba3b80a960ee00cba
Mateusz Murak <mateuszmurak@yahoo.com>
2023-06-16 20:57:14 +0200

    Main logic of getBlockByColor() in Wall.java implemented.

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -2,12 +2,26 @@ package org.example;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.ListIterator;
 
 public class Wall implements Structure {
     private List<Block> blocks;
 
     public Optional<Block> findBlockByColor(String color) {
-        return null;
+        boolean found = false;
+        List<Block> blocksList = CompositeBlock.getBlocks();
+        ListIterator<Block> blockListIterator = blocksList.listIterator();
+
+        while(blockListIterator.hasNext() && !found) {
+            if(blockListIterator.next().getColor() == color)
+                found = true;
+        }
+        
+        if(found == true)
+            return Optional.ofNullable(blockListIterator.previous());
+        else
+            return Optional.empty();
+
     }
 
     public List<Block> findBlocksByMaterial(String material) {
