a0baa29442d7e050d0d2254374ba1128a934fe2c
Mateusz Murak <mateuszmurak@yahoo.com>
2023-06-20 23:54:56 +0200

    Comments added.

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -8,6 +8,10 @@ import java.util.ListIterator;
 public class Wall implements Structure {
     private List<Block> blocks;
 
+    // Method creates iterator on the list of blocks and loops through it.
+    // When match in color is found, the loop stops by setting the "found" flag,
+    // then proceeds to return the last iterated block.
+    // When no match found, method returns optionals "empty()" value.
     public Optional<Block> findBlockByColor(String color) {
         ListIterator<Block> blocksIterator = blocks.listIterator();
         boolean found = false;
@@ -24,6 +28,9 @@ public class Wall implements Structure {
 
     }
 
+    // Method creates iterator on the list of blocks and loops through it.
+    // Everytime match in material is found, the last matched block is added to list of matching blocks.
+    // Method returns an array list, which may be empty if no matches found.
     public List<Block> findBlocksByMaterial(String material) {
         ArrayList<Block> matchingBlocks = new ArrayList<Block>();
         ListIterator<Block> blocksIterator = blocks.listIterator();
@@ -36,6 +43,8 @@ public class Wall implements Structure {
         return matchingBlocks;
     }
 
+    // Method creates iterator on the list of blocks and loops through it,
+    // increasing the counter on every element. Then returns the counter.
     public int count() {
         ListIterator<Block> blocksIterator = blocks.listIterator();
         int blocksCount=0;
